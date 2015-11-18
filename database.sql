ad55957d825212910dda54cd8b01600ca080f829
Nick Whitelegg <nickw4426@gmail.com>
2015-11-18 15:31:04 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+# Setup database
 CREATE TABLE annotations (id serial, wayid INT, text VARCHAR(255), dir INT DEFAULT 0, xy geometry, type VARCHAR(255), authorised INT DEFAULT 0, userid INT DEFAULT 0);
 CREATE TABLE panoramas (id serial, authorised INT DEFAULT 0,userid INT, xy geometry);
 CREATE TABLE users (id serial, username VARCHAR(255), password VARCHAR(255), email VARCHAR(255), k INT, active INT DEFAULT 0, isadmin INT DEFAULT 0);
