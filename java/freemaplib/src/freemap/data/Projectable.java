a54cb0a6769866896941b210b1c67cffdcb9e20d
nickw1 <nick_whitelegg@yahoo.co.uk>
2017-05-11 17:50:26 +0100

    Removed freemaplib: now in its own freemaplib repository

--- java/freemaplib/src/freemap/data/Projectable.java
+++
@@ -1,7 +0,0 @@
-package freemap.data;
-
-public abstract class Projectable {
-	protected Projection proj;
-	
-	public abstract void reproject(Projection newProj);
-}
--- java/freemaplib/src/freemap/datasource/DataInterpreter.java
+++
@@ -1,9 +0,0 @@
-package freemap.datasource;
-
-import java.io.InputStream;
-
-
-// Allows XML and non-XML data sources to have a common interface
-public interface DataInterpreter {
-	public Object getData(InputStream in) throws Exception;
-}
