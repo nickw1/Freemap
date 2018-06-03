2feb9fc12d1f6f3ccea953f23aeeec9d8d3b23f4
Nick Whitelegg <nickw4426@gmail.com>
2018-06-03 10:23:00 +0100

    Controller alterations to allow raw HTTP data to be processed as long as a Sanitiser is specified

--- lib/Controller.php
+++ lib/Controller.php
@@ -1,37 +1,54 @@
 <?php
 require_once('View.php');
+require_once('Sanitiser.php');
 
 class Controller {
-	
-    protected $view, $allowedActions; 
+    
+    protected $view, $allowedActions, $sanitiser;
 
     public function __construct(View $view, $allowedActions=null) {
-		$this->view = $view;
-		$this->allowedActions = $allowedActions;
-	}
+        $this->view = $view;
+        $this->allowedActions = $allowedActions;
+    }
 
     // WARNING should NOT be used without some form of validation of the
     // input data!!!
-	// Basic validation of ID (numeric) and action (letters only) 
+    // Basic validation of ID (numeric) and action (letters only) 
     public function execute($inputData) {
-		$action = isset($inputData["action"]) ? $inputData["action"] :
-			strtolower($_SERVER["REQUEST_METHOD"]);
-		if($action=="get"||$action=="post" ||
-			($this->allowedActions===null || in_array($action,
-				$this->allowedActions)) && 
-			ctype_alpha($action) &&
-			(!isset($inputData["id"]) || is_numeric($inputData["id"]))) {
-        	$function = "action".ucfirst($action);
-           	if(method_exists($this, $function)) {
-            	$this->$function($inputData);
-        	} else {
-				echo "ERROR: Unknown action $action";
-			} 
-		} else {
-			echo "ERROR: Disallowed action, ".
-				"action must be letters and/or ID must be numeric.";
-		}
-		return $action;
+        $action = isset($inputData["action"]) ? $inputData["action"] :
+            strtolower($_SERVER["REQUEST_METHOD"]);
+        if($action=="get"||$action=="post" ||
+            ($this->allowedActions===null || in_array($action,
+                $this->allowedActions)) && 
+            ctype_alpha($action) &&
+            (!isset($inputData["id"]) || is_numeric($inputData["id"]))) {
+            $function = "action".ucfirst($action);
+               if(method_exists($this, $function)) {
+                $this->$function($inputData);
+            } else {
+                $this->actionDefault($inputData);
+            } 
+        } else {
+            echo "ERROR: Disallowed action, action must be letters and/or ID, if supplied, must be numeric.";
+        }
+        return $action;
+    }
+
+    public function rawExecute() {
+        $httpData  = $_SERVER['REQUEST_METHOD']=='POST' ? $_POST: $_GET;
+        if($this->sanitiser) {
+            $this->execute($this->sanitiser->sanitise($httpData));
+        } else {
+            throw new Exception("No sanitiser... refusing to process raw data");
+        }
+    }
+
+    public function actionDefault($inputData) {
+        echo "ERROR: Unknown action";
+    }
+
+    public function setSanitiser(Sanitiser $s) {
+        $this->sanitiser = $s;
     }
 }
 ?>
---
+++ lib/Sanitiser.php
@@ -0,0 +1,5 @@
+<?php
+interface Sanitiser {
+    public function sanitise($inputData);
+}
+?>
